90b4b19662c94f861ba63efed81ee2e6690a1832
xLucaspx <xlucaspx@gmail.com>
2022-10-15 16:58:40 -0300

    Adding App.java and runnable .jar file

---
+++ src/challenge/hotel/main/App.java
@@ -0,0 +1,11 @@
+package challenge.hotel.main;
+
+import challenge.hotel.views.MainMenu;
+
+public class App {
+
+	public static void main(String[] args) {
+		MainMenu mainMenu = new MainMenu();
+		mainMenu.setVisible(true);
+	}
+}
