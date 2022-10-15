d8d11e82b66590fce6900801dda4dff6199e6d9f
xLucaspx <xlucaspx@gmail.com>
2022-10-15 16:38:18 -0300

    Updating DAOs

--- src/challenge/hotel/dao/GuestDAO.java
+++ src/challenge/hotel/dao/GuestDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,31 +19,6 @@ public class GuestDAO {
 	}
 
 	public int register(Guest guest) {
-		String sql = "INSERT INTO hospedes (nome, sobrenome, data_nasc, nacionalidade, telefone) VALUES (?, ?, ?, ?, ?);";
-
-		try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-			statement.setString(1, guest.getName());
-			statement.setString(2, guest.getSurname());
-			statement.setString(3, guest.getBirthDate().toString());
-			statement.setString(4, guest.getNationality());
-			statement.setString(5, guest.getPhone());
-
-			statement.execute();
-
-			try (ResultSet result = statement.getGeneratedKeys()) {
-				while (result.next()) {
-					return result.getInt(1);
-				}
-			}
-
-			return 0;
-
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	public int registerWithReservationId(Guest guest) {
 		String sql = "INSERT INTO hospedes (nome, sobrenome, data_nasc, nacionalidade, telefone, id_reserva) VALUES (?, ?, ?, ?, ?, ?);";
 
 		try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
@@ -68,24 +44,24 @@ public class GuestDAO {
 		}
 	}
 
-	public List<Guest> listaHospedes() {
-		List<Guest> hospedes = new ArrayList<>();
+	public List<Guest> listGuests() {
+		List<Guest> guests = new ArrayList<>();
 
 		String sql = "SELECT id, nome, sobrenome, data_nasc, nacionalidade, telefone, id_reserva FROM hospedes;";
 
 		try (PreparedStatement statement = connection.prepareStatement(sql)) {
 			statement.execute();
 
-			transformaResultSetEmHospedes(statement, hospedes);
+			transformResultSetInGuests(statement, guests);
 
-			return hospedes;
+			return guests;
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public void deletaHospede(int id) {
+	public void deleteGuest(int id) {
 		String sql = "DELETE FROM hospedes WHERE id = ?;";
 
 		try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -97,13 +73,35 @@ public class GuestDAO {
 		}
 	}
 
-	private void transformaResultSetEmHospedes(PreparedStatement statement, List<Guest> hospedes)
-			throws SQLException {
+	public void editGuest(Guest guest) {
+		String sql = "UPDATE hospedes h SET h.nome = ?, h.sobrenome = ?, h.data_nasc = ?, h.nacionalidade = ?, h.telefone = ?, h.id_reserva = ? WHERE id = ?";
+
+		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+			statement.setString(1, guest.getName());
+			statement.setString(2, guest.getSurname());
+			statement.setString(3, guest.getBirthDate().toString());
+			statement.setString(4, guest.getNationality());
+			statement.setString(5, guest.getPhone());
+			statement.setInt(6, guest.getReservationId());
+			statement.setInt(7, guest.getId());
+
+			statement.execute();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void transformResultSetInGuests(PreparedStatement statement, List<Guest> guests) throws SQLException {
 		try (ResultSet result = statement.getResultSet()) {
 			while (result.next()) {
+				String[] birthArray = result.getString(4).split("-");
 
-				hospedes.add(new Guest(result.getInt(1), result.getString(2), result.getString(3),
-						result.getDate(4).toLocalDate(), result.getString(5), result.getString(6), result.getInt(7)));
+				LocalDate birthDate = LocalDate.of(Integer.parseInt(birthArray[0]), Integer.parseInt(birthArray[1]),
+						Integer.parseInt(birthArray[2]));
+
+				guests.add(new Guest(result.getInt(1), result.getString(2), result.getString(3), birthDate,
+						result.getString(5), result.getString(6), result.getInt(7)));
 			}
 		}
 	}
--- src/challenge/hotel/dao/ReservationDAO.java
+++ src/challenge/hotel/dao/ReservationDAO.java
@@ -5,6 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import challenge.hotel.models.Reservation;
 
@@ -23,18 +26,82 @@ public class ReservationDAO {
 			statement.setString(2, reservation.getCheckOutDate().toString());
 			statement.setString(3, reservation.getValue());
 			statement.setString(4, reservation.getPaymentMethod());
-			
+
 			statement.execute();
-			
+
 			ResultSet result = statement.getGeneratedKeys();
 			while (result.next()) {
 				return result.getInt(1);
 			}
-			
+
 			return 0;
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
+		}
+	}
+
+	public List<Reservation> listReservations() {
+		List<Reservation> reservations = new ArrayList<>();
+
+		String sql = "SELECT id, data_entrada, data_saida, valor, forma_pagamento FROM reservas;";
+
+		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+			statement.execute();
+
+			transformResultSetInReservations(statement, reservations);
+
+			return reservations;
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void deleteReservation(int id) {
+		String sql = "DELETE FROM reservas WHERE id = ?;";
+
+		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+			statement.setInt(1, id);
+			statement.execute();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void editReservation(Reservation reservation) {
+		String sql = "UPDATE reservas r SET r.data_entrada = ?, r.data_saida = ?, r.valor = ?, r.forma_pagamento = ? WHERE id = ?";
+		
+		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+			statement.setString(1, reservation.getCheckInDate().toString());
+			statement.setString(2, reservation.getCheckOutDate().toString());
+			statement.setString(3, reservation.getValue());
+			statement.setString(4, reservation.getPaymentMethod());
+			statement.setInt(5, reservation.getId());
+			
+			statement.execute();
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void transformResultSetInReservations(PreparedStatement statement, List<Reservation> reservations)
+			throws SQLException {
+		try (ResultSet result = statement.getResultSet()) {
+			while (result.next()) {
+				String[] checkInArray = result.getString(2).split("-");
+				String[] checkOutArray = result.getString(3).split("-");
+
+				LocalDate checkInDate = LocalDate.of(Integer.parseInt(checkInArray[0]),
+						Integer.parseInt(checkInArray[1]), Integer.parseInt(checkInArray[2]));
+				LocalDate checkOutDate = LocalDate.of(Integer.parseInt(checkOutArray[0]),
+						Integer.parseInt(checkOutArray[1]), Integer.parseInt(checkOutArray[2]));
+
+				reservations.add(new Reservation(result.getInt(1), checkInDate, checkOutDate, result.getString(4),
+						result.getString(5)));
+			}
 		}
 	}
 }
